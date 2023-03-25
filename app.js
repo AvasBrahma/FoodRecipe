0525d5bb6077146d161bffeb4d5c9539c1bf57fc
AvasBrahma <avasbrahma@gmail.com>
2023-03-25 16:40:53 +0530

    added js code to fetch data for recipe details

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const searchBtn=document.getElementById('search-btn');
 const mealList=document.getElementById('meal');
+const mealDetailsContent=document.querySelector('.meal-details-content');
 
+const recipeCloseBtn=document.getElementById('recipe-close-btn');
 
 searchBtn.addEventListener('click', getMealList);
+mealList.addEventListener('click', getMealRecipe);
+recipeCloseBtn.addEventListener('click', ()=>{
+    mealDetailsContent.parentElement.classList.remove('showRecipe');
+})
+
+
+
 
 //get meal list that matches with the ingredirets
 
@@ -29,8 +38,49 @@ function getMealList(){
                 `;
                 
             });
+            mealList.classList.remove('notFound');
+        } else{
+            html= "Sorry, we didn't find any meal";
+            mealList.classList.add('notFound');
         }
         mealList.innerHTML=html;
-
      })
+}
+
+//get recipe of the meal
+
+function getMealRecipe(e){
+    e.preventDefault();
+    if(e.target.classList.contains('recipe-btn')){
+        let mealItem=e.target.parentElement.parentElement;
+        console.log(mealItem);
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+        .then(response=>response.json())
+        .then(data=>mealRecipeModal(data.meals));
+    }
+}
+
+//create Modal 
+function mealRecipeModal(meal){
+    console.log(meal);
+    meal=meal[0];
+    let html=`
+    <h2 class="recipse-tile">${meal.strMeal}</h2>
+                        <p class="recipe-category">${meal.strCategory}</p>
+                        <div class="recipe-instruct">
+                            <h3>Instructions</h3>
+                            <p>${meal.strInstructions}</p>
+                        </div>
+                        <div class="recipe-meal-img">
+                             <img src="${meal.strMealThumb}" alt="">
+                        </div>
+                         <div class="recipe-link">
+                            <a href="${meal.strYoutube}" target="_blank">Watch Video</a>
+                         </div>
+                     </div>
+
+    `;
+
+    mealDetailsContent.innerHTML=html;
+    mealDetailsContent.parentElement.classList.add('showRecipe');
 }
