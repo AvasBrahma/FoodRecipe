539399bddf1c3b70ffa51f56c574b8792a21bdbb
AvasBrahma <avasbrahma@gmail.com>
2023-03-25 15:52:51 +0530

    added html and css for recipe details section

---
+++ app.js
@@ -0,0 +1,36 @@
+const searchBtn=document.getElementById('search-btn');
+const mealList=document.getElementById('meal');
+
+
+searchBtn.addEventListener('click', getMealList);
+
+//get meal list that matches with the ingredirets
+
+function getMealList(){
+    let searchInputTxt=document.getElementById('search-input').value.trim();
+     console.log(searchInputTxt);
+
+     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+     .then(response=>response.json())
+     .then(data=>{
+        let html="";
+        if(data.meals){
+            data.meals.forEach(meal => {
+                html +=`
+                <div class="meal-item" data-id="${meal.idMeal}">
+                   <div class="meal-img">
+                      <img src="${meal.strMealThumb}" alt="food">
+                   </div>
+                   <div class="meal-name">
+                       <h3>${meal.strMeal}</h3>
+                       <a href="#" class="recipe-btn">Get Recipe</a>
+                  </div>
+               </div>
+                `;
+                
+            });
+        }
+        mealList.innerHTML=html;
+
+     })
+}
